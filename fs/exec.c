975ed8ca3fb5f122898ad5ff7f8a7461120271e9
Nady <103646211+mohamedamrnady@users.noreply.github.com>
2023-04-17 20:17:14 +0200

    adapt for kernelsu

--- fs/exec.c
+++ fs/exec.c
@@ -1705,6 +1705,8 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1716,6 +1718,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
